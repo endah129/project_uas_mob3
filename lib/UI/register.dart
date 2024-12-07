ed5ddaa25315fa553bb150533cd1276a9548b362
Riki Sanjaya <imaderikiwidiastanasanjaya@gmail.com>
2024-12-07 22:26:03 +0800

    fix firebase initialization issue; fix all flutter warnings

--- lib/UI/register.dart
+++ lib/UI/register.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:mob3_login_022_endah/auth/serviceAuth.dart';
+import 'package:mob3_login_022_endah/auth/service_auth.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key, required String title});
 
   @override
-  _RegisterState createState() => _RegisterState();
+  State<Register> createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
@@ -14,16 +14,14 @@ class _RegisterState extends State<Register> {
   final TextEditingController _passwordController2 = TextEditingController();
   // final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  String _errorMessage = "";
+  final String _errorMessage = ""; // ini variable ga pernah berubah
 
-  Future<void> _login() async {
-
-  }
+  // Future<void> _login() async {} // ini ga ngapa2in
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Daftar Sekarang")),
+      appBar: AppBar(title: const Text("Daftar Sekarang")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -31,30 +29,28 @@ class _RegisterState extends State<Register> {
           children: [
             TextField(
               controller: _emailController2,
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
             TextField(
               controller: _passwordController2,
               obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             if (_errorMessage.isNotEmpty)
               Text(
                 _errorMessage,
-                style: TextStyle(color: Colors.red),
+                style: const TextStyle(color: Colors.red),
               ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-                await AuthService().signup(
+                await signup(
                     email: _emailController2.text,
                     password: _passwordController2.text,
-                    context: context
-                  );
-                },
-              child: Text("Daftar"),
-              
+                    context: context);
+              },
+              child: const Text("Daftar"),
             ),
           ],
         ),
